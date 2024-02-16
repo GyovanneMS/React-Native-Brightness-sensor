8dbf449d1f9b0dfcd280bdf1d913cd31e2eab9f3
GyovanneMS <gyovannecontato@gmail.com>
2024-02-16 14:50:00 -0300

    Project

--- App.js
+++ App.js
@@ -1,13 +1,42 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React,{useState, useEffect} from 'react';
+import { StyleSheet, Text, View, StatusBar, ScrollView, Image } from 'react-native';
+import { LightSensor } from 'expo-sensors';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+
+  const [{illuminance}, definirIluminacao] = useState({illuminance: 0})
+
+  useEffect(function(){
+    LightSensor.addListener(definirIluminacao)
+  }, [])
+
+  function VerificarLuz(escuro, claro){
+    return illuminance > 20 ? escuro : claro
+  }
+//https://images.app.goo.gl/xkdbUpdq6JnhViSWA
+  return <ScrollView
+    style = {{backgroundColor: VerificarLuz("#FFFBEB", "#20262#")}}>
+    <StatusBar barStyle={VerificarLuz("dark-content", "light-content")} backgroundColor={VerificarLuz("#FFFBEB", "#000000")}/>
+    <View>
+      <Text>Sensor do Smartphone!!!</Text>
+
+      <Text>Testestando iluminação do ambiente!</Text>
+
+      <Text> {illuminance} </Text>
+
+      <Image 
+        source={{uri: VerificarLuz("https://static.todamateria.com.br/upload/15/83/1583748930ic5e661742ed9ab-og.jpg?class=ogImageWide", "https://www.reddit.com/media?url=https%3A%2F%2Fi.redd.it%2Fnl1j7k2821vb1.jpeg&rdt=39218")}}
+        style={{height: 200}}
+      />
+
+
+
     </View>
-  );
+
+
+
+
+  </ScrollView>
 }
 
 const styles = StyleSheet.create({
